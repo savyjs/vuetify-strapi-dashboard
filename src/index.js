45add1e84af7e9c0e7d427509f0e0686c12099c9
savvy <savvyversa@gmail.com>
2021-01-22 01:54:36 +0330

    add popUp

--- src/index.js
+++ src/index.js
@@ -1,36 +1,38 @@
-
 // Import vue component
 import ListPage from './common-ssr/ListPage';
+import PopUp from './common-ssr/PopUp';
 
 // install function executed by Vue.use()
 function install(Vue) {
-  if (install.installed) return;
-  install.installed = true;
-  Vue.component('ListPage', ListPage);
+    if (install.installed) return;
+    install.installed = true;
+    Vue.component('ListPage', ListPage);
+    Vue.component('PopUp', PopUp);
 }
 
 // Create module definition for Vue.use()
 const plugin = {
-  install,
+    install,
 };
 
 // To auto-install when vue is found
 let GlobalVue = null;
 if (typeof window !== 'undefined') {
-  GlobalVue = window.Vue;
+    GlobalVue = window.Vue;
 } else if (typeof global !== 'undefined') {
-  GlobalVue = global.Vue;
+    GlobalVue = global.Vue;
 }
 if (GlobalVue) {
-  GlobalVue.use(plugin);
+    GlobalVue.use(plugin);
 }
 
 // Inject install function into component - allows component
 // to be registered via Vue.use() as well as Vue.component()
 ListPage.install = install;
+PopUp.install = install;
 
 // Export component by default
-export default ListPage;
+export default {ListPage, PopUp};
 
 // It's possible to expose named exports when writing components that can
 // also be used as directives, etc. - eg. import { RollupDemoDirective } from 'rollup-demo';
