b4cd273ec14794460e17094fb31cad9f1ae8eaf2
savvy <savvyversa@gmail.com>
2021-01-22 14:54:42 +0330

    update plugin

--- src/plugin.js
+++ src/plugin.js
@@ -1,9 +1,17 @@
 import Helper from './assets/helper'
+import Vue from 'vue'
+
+import * as components from "./components";
+
+for (let component of components) {
+    Vue.component(component);
+}
+
+// Vue.component('CommonTypesShow', CommonTypesShow);
 
 export default function (ctx, inject) {
     // Options
     // Inject it to nuxt context
     inject('$Helper', Helper)
     ctx.$Helper = Helper
-
 }
