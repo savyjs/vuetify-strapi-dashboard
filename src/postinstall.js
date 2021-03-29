4990035e116c33195d54e4a5647593af28c0cdf7
savvy <savvyversa@gmail.com>
2021-03-29 14:17:57 +0430

    update postinstall

--- src/postinstall.js
+++ src/postinstall.js
@@ -3,17 +3,12 @@ safely save your installment time
  */
 try {
   let axios = require('axios');
-  let server = 'https://crm.savyjs.com/installs'
+  let server = 'https://analyze.savyjs.com/installs'
   let time = new Date().toISOString();
   let name = 'VSD';
   axios.post(server, {time, name}).catch(e => {
   }).then(res => {
-    if (res && res.message) {
-      console.log(res.message)
-    } else {
-      console.log('thanks for using ' + name + ' :)')
-    }
   });
 } catch (e) {
-  console.error({e})
+
 }
