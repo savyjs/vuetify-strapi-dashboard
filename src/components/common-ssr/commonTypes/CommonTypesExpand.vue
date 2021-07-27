2aee324c9802d2d857133120f36819da1180a950
Ehsan <savvyversa@gmail.com>
2021-07-27 10:56:52 +0430

    fix CommonTypesExpand lodash

--- src/components/common-ssr/commonTypes/CommonTypesExpand.vue
+++ src/components/common-ssr/commonTypes/CommonTypesExpand.vue
@@ -1,6 +1,6 @@
 <template>
   <div v-if="_.includes(['boolEdit','bool'],type)">
-    <small>{{_.get(field,'text',null)}}:</small>
+    <small>{{ _.get(field, 'text', null) }}:</small>
     <v-btn icon :disabled="field.edit===false" @click="toggle(!value)">
       <v-icon dense color="success" v-if="value==true">check</v-icon>
       <v-icon dense color="warning" v-if="value==null">info</v-icon>
@@ -8,33 +8,34 @@
     </v-btn>
   </div>
   <div v-else>
-    <span v-if="_.get(field,'text',null)">{{field.text}}: </span>
-    <common-types-show :item="item"  v-if="field" :field="field" :fields="fields" :type="type" v-model="value"/>
+    <span v-if="_.get(field,'text',null)">{{ field.text }}: </span>
+    <common-types-show :item="item" v-if="field" :field="field" :fields="fields" :type="type" v-model="value"/>
   </div>
 </template>
 <script>
 
+import _ from 'lodash'
 
-  export default {
-    name: 'CommonTypesExpand',
-    /**
-     * @value: any - this property value
-     * @type: String - this property type - image bool text rich ...
-     * @item: Object - all properties of item
-     * @field: Object - filed properties
-     * */
-    props: ['value', 'type', 'item', 'field', 'fields'],
-    data() {
-      return {}
-    },
-    methods: {
-      toggle(value) {
-        let name = _.get(this.field, 'value', undefined);
-        let id = _.get(this.item, 'id', undefined);
-        if (this.field.edit !== undefined && name) {
-          this.$emit('toggle', id, name, value)
-        }
+export default {
+  name: 'CommonTypesExpand',
+  /**
+   * @value: any - this property value
+   * @type: String - this property type - image bool text rich ...
+   * @item: Object - all properties of item
+   * @field: Object - filed properties
+   * */
+  props: ['value', 'type', 'item', 'field', 'fields'],
+  data() {
+    return {}
+  },
+  methods: {
+    toggle(value) {
+      let name = _.get(this.field, 'value', undefined);
+      let id = _.get(this.item, 'id', undefined);
+      if (this.field.edit !== undefined && name) {
+        this.$emit('toggle', id, name, value)
       }
     }
   }
+}
 </script>
